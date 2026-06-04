5c77d16e78d408a5e30b0aa4156e75cb56e9cd9e
Al Amin <alamin876123@gmail.com>
2026-06-05 00:47:57 +0600

    chore: improve Django settings configuration

--- detector_project/settings.py
+++ detector_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5!&wchiu6&ak-6o=t50tnhl#***!q57erohc1fu3207++ss!r6'
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "dev-only-secret-key-for-local-assignment"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
