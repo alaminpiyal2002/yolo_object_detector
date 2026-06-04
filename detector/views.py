284d300102469c76d51c57cda8fd9dfcab4b49b6
Al Amin <alamin876123@gmail.com>
2026-06-04 22:08:30 +0600

    feat: add Bootstrap on  upload homepage

--- detector/views.py
+++ detector/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("YOLO Object Detector homepage is working.")
+    return render(request, "detector/index.html")
